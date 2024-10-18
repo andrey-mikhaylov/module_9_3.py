55100e2e334bb4f19d0bb80ff74ef339dafdf941
Andrey <andrey.mikhaylov.n@gmail.com>
2024-10-18 14:07:13 +0300

    done

--- module_9_3.py
+++ module_9_3.py
@@ -4,11 +4,11 @@ def test():
 
     # В переменную first_result запишите генераторную сборку, которая высчитывает разницу длин строк из списков first и second,
     # если их длины не равны. Для перебора строк попарно из двух списков используйте функцию zip.
-    first_result = ()
+    first_result = (len(s1) - len(s2) for s1, s2 in zip(first, second) if len(s1) != len(s2))
 
     # В переменную second_result запишите генераторную сборку, которая содержит результаты сравнения длин строк
     # в одинаковых позициях из списков first и second. Составьте эту сборку НЕ используя функцию zip. Используйте функции range и len.
-    second_result = ()
+    second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
 
     print(list(first_result))
     print(list(second_result))
